5f00ca290b9287779557a2d2c786bf2c371002e4
dacerron <dacerron@gmail.com>
2021-11-24 13:49:31 -0800

    timer working with color change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
   ARCanvas,
   DefaultXRControllers,
 } from '@react-three/xr'
-import { useFrame, useThree, extend } from '@react-three/fiber'
+import { useFrame, useThree, extend, Canvas } from '@react-three/fiber'
 import { Box } from '@react-three/drei'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './styles.css'
@@ -24,6 +24,7 @@ import Compass from './Components/Compass.js'
 import JudgeAvatar from './Components/JudgeAvatar.js'
 import Player from './Components/Player.js'
 import SettingsPage from './Components/SettingsPage'
+import Timer from './Components/Timer.js'
 
 extend({ OrbitControls })
 
@@ -62,39 +63,41 @@ function App() {
   return (
     <>
       {presentationStarted ?
-        <VRCanvas>
-          <Compass ></Compass>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Hands
-          // modelLeft={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/left-hand-black-webxr-tracking-ready/model.gltf"}
-          // modelRight={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/right-hand-black-webxr-tracking-ready/model.gltf"}
-          />
-          {
-            //<Avatar position={[-3,-4, -2]} rotation={[0, 0.8, 0]} buttonOffset={[-2, 4, 0]} modelUrl={"./models/testvid_default.glb"}/>
-          }
-          <JudgeAvatar position={[-2, -2, -4]}  utteranceSplit={180000} />
-          
-          <JudgeAvatar position={[0, -2, -4]}  utteranceSplit={180000} />
-          
-          <JudgeAvatar position={[2, -2, -4]}  utteranceSplit={180000} />
-          <Model modelUrl="./models/courtroompropsNov17.glb"
-            pos={[0, -3, 3.5]}
-            rot={[0, 0, 0]}
-            sca={[0.06, 0.06, 0.06]} />
-          <Model modelUrl="./models/courtroomwallsNov17.glb"
-            pos={[0, -3, 3.5]}
-            rot={[0, 0, 0]}
-            sca={[0.06, 0.06, 0.06]} />
-          <Model modelUrl="./models/courtroomdesksNov17.glb"
-            pos={[0, -3, 3.5]}
-            rot={[0, 0, 0]}
-            sca={[0.06, 0.06, 0.06]} />
-          <DefaultXRControllers />
-          <Player startPosition={[0, 0.5, 0]} />
-        </VRCanvas> : <>
+        <>
+          <VRCanvas style={{position: "absolute"}}>
+            <Compass ></Compass>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Hands
+            // modelLeft={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/left-hand-black-webxr-tracking-ready/model.gltf"}
+            // modelRight={"https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/right-hand-black-webxr-tracking-ready/model.gltf"}
+            />
+            {
+              //<Avatar position={[-3,-4, -2]} rotation={[0, 0.8, 0]} buttonOffset={[-2, 4, 0]} modelUrl={"./models/testvid_default.glb"}/>
+            }
+            <JudgeAvatar position={[-2, -2, -4]} utteranceSplit={180000} />
+
+            <JudgeAvatar position={[0, -2, -4]} utteranceSplit={180000} />
+
+            <JudgeAvatar position={[2, -2, -4]} utteranceSplit={180000} />
+            <Model modelUrl="./models/courtroompropsNov17.glb"
+              pos={[0, -3, 3.5]}
+              rot={[0, 0, 0]}
+              sca={[0.06, 0.06, 0.06]} />
+            <Model modelUrl="./models/courtroomwallsNov17.glb"
+              pos={[0, -3, 3.5]}
+              rot={[0, 0, 0]}
+              sca={[0.06, 0.06, 0.06]} />
+            <Model modelUrl="./models/courtroomdesksNov17.glb"
+              pos={[0, -3, 3.5]}
+              rot={[0, 0, 0]}
+              sca={[0.06, 0.06, 0.06]} />
+            <DefaultXRControllers />
+            <Player startPosition={[0, 0.5, 0]} />
+          </VRCanvas> <Timer isPresentationStarted={presentationStarted}></Timer></> : <>
           <button type="button" onClick={() => { setPresentationStarted(true) }}>Start Experience</button>
           <button type="button" onClick={() => { setSettingsPageActive(true) }}>Settings Page</button>
+
           {settingsPageActive ? <SettingsPage hideSettingsPage={hideSettingsPage}></SettingsPage> : null}</>}
     </>
   );
