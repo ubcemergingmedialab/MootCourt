d41450455b2a9502382387ea750c02c38063d517
Olivia Chen <ochen@Olivias-MBP.hitronhub.home>
2021-12-09 15:46:04 -0800

    Implemented work flow

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,13 +1,11 @@
-import SetupPage from "./SetupPage"
+import './homepageStyles.css'
 
-
-function HomePage({SetupPage}){
+function HomePage({setupPage}){
     return(<>
         <div>
             <h1>Moot Court</h1>
-            <button onClick={()=> {SetupPage()}}> Start Moot Practice</button>
+            <button onClick={()=> {setupPage()}}> Start Moot Practice</button>
             <button>Resources</button>
-            <button>Settings</button>
         </div>
     </>)
 }
