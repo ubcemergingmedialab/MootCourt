ea5078a9b6844368092bb0aecbb7b2e73343395f
dacerron <dacerron@gmail.com>
2022-01-13 15:27:52 -0800

    is deleting last question

--- src/Components/AddQuestionsPopup.jsx
+++ src/Components/AddQuestionsPopup.jsx
@@ -1,39 +1,31 @@
 import { useState } from 'react'
 import CustomQuestion from './CustomQuestion'
 import './addQuestionsStyles.css'
-import { useEffect } from 'react/cjs/react.development'
 
 
 function AddQuestionsPopup({ disable }) {
-    const [questions, setQuestions] = useState(["new question"])
-    const [renderedQuestions, setRenderedQuestions] = useState(["new question"])
+    const [questions, setQuestions] = useState({ 1: "new question" })
     const updateQuestionHandler = (index) => {
         return function (value) {
-            let tempQuestions = questions
-            questions[index] = value
-            setQuestions(tempQuestions)
+            let tempQuestions = {...questions}
+            tempQuestions[index] = { value: value }
+            //console.log(tempQuestions)
+            setQuestions({...tempQuestions})
         }
     }
 
     const deleteQuestionHandler = function (i) {
-        let tempQuestions = questions
-        tempQuestions.splice(i, 1)
-        setQuestions(tempQuestions)
+        let tempQuestions = { ...questions }
+        delete tempQuestions[i]
+        console.log(tempQuestions)
+        setQuestions({...tempQuestions})
     }
 
     const submitQuestionHandler = function (i) {
-        if (i >= (questions.length - 1)) {
-            console.log('pushing question')
-            let tempQuestions = questions
-            tempQuestions.push("new")
-            setQuestions(tempQuestions)
-        }
+        let tempQuestions = { ...questions }
+        tempQuestions[i + 1] = "new question"
+        setQuestions({...tempQuestions})
     }
-
-    useEffect(() => {
-        console.log(questions)
-        setRenderedQuestions(questions)
-    }, [questions])
 
     return (<>
         <div className={"popup-box"}>
@@ -41,13 +33,13 @@ function AddQuestionsPopup({ disable }) {
             <h1>Will need to see how this can be a popup (modal?)</h1>
             <button type="button" onClick={() => { disable() }}>x</button>
             <ul>
-                {renderedQuestions.map((e, i) => {
+                {Object.keys(questions).map((e, i) => {
                     return (
-                        <li key={"questionListItem-" + i} id={"questionListItem-" + i}>
-                            <CustomQuestion key={"customQuestion-" + i} id={"customQuestion-" + i}
-                                updateQuestion={updateQuestionHandler(i)}
-                                deleteQuestion={() => { deleteQuestionHandler(i) }}
-                                submitQuestion={() => { submitQuestionHandler(i) }}>
+                        <li key={"questionListItem-" + e} id={"questionListItem-" + e}>
+                            <CustomQuestion key={"customQuestion-" + e} id={"customQuestion-" + e}
+                                updateQuestion={updateQuestionHandler(e)}
+                                deleteQuestion={() => { deleteQuestionHandler(e) }}
+                                submitQuestion={() => { submitQuestionHandler(e) }}>
                             </CustomQuestion></li>)
                 })}
             </ul>
