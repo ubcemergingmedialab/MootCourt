2c94dcf25667c14b737ec53b62d5247c0200bac7
Olivia Chen <ochen@Olivias-MacBook-Pro.local>
2022-01-13 14:04:11 -0800

    Input added to addQuestions.jsx

--- src/Components/AddQuestionsPopup.jsx
+++ src/Components/AddQuestionsPopup.jsx
@@ -8,6 +8,7 @@ function AddQuestionsPopup({}){
             <h1>This is a separate component to add questions for now, but it may be a popup later</h1> 
             <h1>Will need to see how this can be a popup (modal?)</h1>
         </div>
+        <input type="text" />
     
     </>)
 }
