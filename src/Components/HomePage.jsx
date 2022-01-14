383e5ae0347318a4d40a162bfb5c4efea6b9f29e
veronicna <verochen.sr@gmail.com>
2022-01-14 14:09:09 -0800

    Styled the landing and home page

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -2,11 +2,25 @@ import './homepageStyles.css'
 
 function HomePage({setupPage}){
     return(<>
-        <div>
-            <h1>Moot Court</h1>
-            <button onClick={()=> {setupPage()}}> Start Moot Practice</button>
-            <button>Resources</button>
-        </div>
+        <body id="Home">
+            <div id="Rectangle">
+            </div>
+            <div id="RectangleMid">
+            </div>
+            <div id="TitleBorder">
+                <h1>Moot Court</h1>
+            </div>
+            <div id="RectangleStart">
+            </div>
+            <div id="RectangleResources">
+            </div>
+            <div id="Start">
+                <button id="StartButton" onClick={()=> {setupPage()}}> Start Moot Practice</button>
+            </div>
+            <div id="Resources">
+                <button id="ResourcesButton">Resources</button>
+            </div>
+        </body>
     </>)
 }
 
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -2,10 +2,17 @@ import './landingpageStyles.css'
 
 function LandingPage({homePage}){
     return(<>
-        <div>
-            <h1>Moot court simulator to help you practice!</h1>
-            <button onClick={()=> {homePage()}}>Click me to begin</button>
-        </div>
+        <body id="Land">
+            <div id="HeaderBorder">
+                <h1>Moot Court</h1> 
+            </div>
+            <div id="SubBorder">
+                <h2>Moot court simulator to help you practice!</h2>
+            </div>
+            <div id="Begin">
+                <button id="BeginButton" onClick={()=> {homePage()}}>Click me to begin</button>
+            </div>
+        </body>
     </>)
 }
 
