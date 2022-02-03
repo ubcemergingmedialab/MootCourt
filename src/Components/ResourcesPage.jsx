c9235e152afa4dd730b6c4970f3528009da28b1b
veronicna <verochen.sr@gmail.com>
2022-02-02 17:45:27 -0800

    Finished Styling Resources and add links

--- src/Components/ResourcesPage.jsx
+++ src/Components/ResourcesPage.jsx
@@ -7,7 +7,19 @@ function ResourcesPage({homePage}){
                 <h1>Moot Resources</h1> 
             </div>
         <div>
-            <button className="button-type1" onClick={() => {homePage()}}>Back</button>
+            <button className="button-type1" onClick={() => {homePage()}}>Back to Home</button>
+        </div>
+        <div id="Subtitle">
+            <p>Here we can find mooting resources to best help you strengthen your mooting skills! </p>
+        </div>
+        <div id="Checklist">
+            <h1><a href="First-Year Moots Procedure Checklist 2020.pdf" target="_blank">First year Moot procedure checklist</a></h1>
+        </div>
+        <div id="Compiled">
+            <h1><a href="Compiled Moot Resources.pdf" target="_blank">General Mooting tips and oral argument criteria</a></h1>
+        </div>
+        <div id="Addressing">
+            <h1><a href="(CandC)Addressing_the_Court_Civil_and_Criminal.pdf" target="_blank">Addressing the court civil and criminal</a></h1>
         </div>
     </>)
 }
