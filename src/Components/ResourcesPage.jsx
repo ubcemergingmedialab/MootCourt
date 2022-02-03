6c4aaeed40e1cec9e7e465de8e725fa3b090acf4
Olivia Chen <ochen@Olivias-MacBook-Pro.local>
2022-02-02 17:52:07 -0800

    a minor change

--- src/Components/ResourcesPage.jsx
+++ src/Components/ResourcesPage.jsx
@@ -5,7 +5,6 @@ function ResourcesPage({homePage}){
     return(<>
         <div>
             <h1>Some words</h1>
-            <button className="button-type1" onClick={() => {homePage()}}>Back</button>
         </div>
     </>)
 }
