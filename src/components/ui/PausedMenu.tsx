91c0ebadd88d39ef7acfd09e1aa8a5d132a8b8f4
MohsenM <dev.movahedi@gmail.com>
2024-02-05 14:53:51 -0800

    added the question's text to the pause menu under Questions button.

--- src/components/ui/PausedMenu.tsx
+++ src/components/ui/PausedMenu.tsx
@@ -77,9 +77,9 @@ const PausedMenu = ({ updateAppState, appPaused, togglePause, children, config }
             <button className="button paused-menu-buttons" type="button" onClick={handleTogglePause}>
             Continue Session
             </button>
-            {/* <button className="button paused-menu-buttons" type="button" onClick={() => resetDisplayedUI('pausedmenucontents', 'paused-menu-transcript')}>
-            Transcript
-            </button> */}
+            <button className="button paused-menu-buttons" type="button" onClick={() => resetDisplayedUI('pausedmenucontents', 'paused-menu-transcript')}>
+            Questions
+            </button>
             <button className="button paused-menu-buttons" type="button" onClick={() => resetDisplayedUI('pausedmenucontents', 'paused-menu-tutorial')}>
             Tutorial
             </button>
@@ -113,16 +113,35 @@ const PausedMenu = ({ updateAppState, appPaused, togglePause, children, config }
             </button>
             </div>
             <div id="paused-menu-transcript" style={{ display: 'none' }}>
-                {displayConversationValue(config={config})}
-
+                {/*displayConversationValue(config={config})*/}
+                <ul>
+                    <li> Questions for Appellants:
+                        <ol>
+                            <li>The trial judge found that the store employee closed the door to the office and stood with his back to it. Can you explain me to why you say that didn't prevent the Respondent and his children from leaving? </li>
+                            <li>Why do you say there was no psychological imprisonment in this case?</li>
+                            <li>Can you please tell me where you are in your factum?</li>
+                            <li>The evidence was that the store employee didn't intervene to avoid the problem when he saw that some of the grocery items hadn't been scanned. In light of that, I am troubled by finding the employee's conduct justified under s. 494(1). What do you have to say about that?</li>
+                            <li>Shouldn't we be worried about giving private security personnel the power to detain members of the public whenever they want?</li>
+                        </ol>
+                    </li>
+                    <li> Questions for Respondents:
+                        <ol>
+                            <li>Can you remind me what the trial judge's findings were regarding who pushed the shopping cart into the office near the Store exit? </li>
+                            <li>Doesn't the fact that the Respondent and his children ultimately left the store on their own demonstrate that he always had that option open to him?</li>
+                            <li>If the Respondent was on his way out of the store with goods he hadn't paid for, wasn't he in the process of committing an offence?</li>
+                            <li>The trial judge concluded that s. 494(1)(b) did not apply. Can you tell us why you do or don't agree with that conclusion?  </li>
+                            <li>Couldn't the Respondent have avoided the whole situation if he produced his receipt when he was first asked to do so?</li>
+                        </ol>
+                    </li>
+                </ul>
                
                 <button className="button paused-menu-buttons" type="button" onClick={() => resetDisplayedUI('paused-menu-transcript', 'pausedmenucontents')}>
                 Back
                 </button>
-                </div> 
-                <div id="paused-menu-transcript" style={{ display: 'none' }}>
+              </div> 
+            {/*<div id="paused-menu-transcript" style={{ display: 'none' }}>
                 {displayConversationValue(config={config})}
-            </div>
+            </div>*/}
         </div>
     );
 };
